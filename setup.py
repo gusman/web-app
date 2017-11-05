f29eae0d6dfcd158f29032fb60cbec3e40d9f4ac
Gusman Dharma P <gusman.dp@gmail.com>
2017-11-05 22:27:06 +0700

    Package structure

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='flaskr',
+    package=['flaskr'],
+    include_package_data=True,
+    install_requires=[
+        'flask',
+    ],
+)
+
