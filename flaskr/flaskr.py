ad7cd8d43ba23fa2131af42eab89a0a61efe76e4
Gusman Dharma P <gusman.dp@gmail.com>
2017-11-05 22:27:33 +0700

    add database handler

--- flaskr/flaskr.py
+++ flaskr/flaskr.py
@@ -12,8 +12,80 @@ app.config.from_object(__name__)
 app.config.update(dict(
     DATABASE = os.path.join(app.root_path, 'flaskr.db'),
     SECRET_KEY = 'development key',
-    USERNAME   = 'admin'
-    PASSOWRD   = 'default'
+    USERNAME = 'admin',
+    PASSWORD = 'default'
 ))
 
-app.config.from_envvar('FLASKR_SETTINGS', silen=True)
+app.config.from_envvar('FLASKR_SETTINGS', silent=True)
+
+def connect_db():
+    """Connects to the specific databse. """
+    rv = sqlite3.connect(app.config['DATABASE'])
+    rv.row_factory = sqlite3.Row
+    return rv
+
+def init_db():
+    db = get_db()
+    with app.open_resource('schema.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+
+@app.cli.command('initdb')
+def initdb_command():
+    """Initializes the databse."""
+    init_db()
+    print('Initialized the database.')
+
+def get_db():
+    """Opens a new database connection if there is none yet for the currect
+    application context.
+    """
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+@app.teardown_appcontext
+def close_db(error):
+    """Closes the database again at the end fo the request."""
+
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
+
+@app.route('/')
+def show_entries():
+    db = get_db()
+    cur = db.execute('select title, text from entries order by id desc')
+    entries = cur.fetchall()
+    return render_template('show_entries.html', entries=entries)
+
+@app.route('/add', methods=['post'])
+def add_entry():
+    if not session.get('logged_in'):
+        abort(401)
+
+    db = get_db()
+    db.execute('Insert into entries (title, text) values (?, ?)',
+                [request.form['title'], request.form['test']])
+    db.commit()
+    flash('New entry was successfully posted')
+    return redirect(url_For('show_entries'))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if 'POST' == request.method:
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return redirect(url_for('show_entries'))
+    return render_template('login.html', error=error)
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('show_entries'))
